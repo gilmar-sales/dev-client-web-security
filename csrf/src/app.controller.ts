62000b49296f905b741c9738e7f271f7b9055529
gilmar-sales <gilmar.custodio@outlook.com.br>
2024-10-29 21:36:48 -0300

    Adicionando pagina de login ao projeto CSRF

--- csrf/src/app.controller.ts
+++ csrf/src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Req, Res } from '@nestjs/common';
+import { Response, Request } from 'express'
+
 import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getIndex(@Req() request: Request) {
+    return { login: request.cookies.login };
   }
+
+  @Get('login')
+  @Render('login')
+  getLogin() {
+    return { message: 'Hello world!' };
+  }
+
+  @Post('login')
+  login(@Body() credencial: { username: string, password: string }, @Res() response: Response) {
+    if (credencial.username === 'admin' &&
+      credencial.password === 'admin'
+    ) {
+      return response.cookie('token', 'imagine um token importante aqui').cookie('login', credencial.username).redirect('/');
+    }
+
+    return response.status(401).send();
+  }
+
 }
