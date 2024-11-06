d35f0afabb9bdee8aca036364ce5646ac3b3898a
Gilmar Sales <gilmar.custodio@outlook.com.br>
2024-11-06 00:25:03 +0000

    Adicionando endpoint para deslogar

--- csrf/src/app.controller.ts
+++ csrf/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post, Render, Req, Res } from '@nestjs/common';
-import { Response, Request } from 'express';
+import { Response, Request, CookieOptions } from 'express';
 
 import { AppService } from './app.service';
 
@@ -9,8 +9,11 @@ export class AppController {
 
   @Get()
   @Render('index')
-  getIndex(@Req() request: Request) {
-    return { login: request.cookies.login };
+  getIndex(@Req() request: Request, @Res() response: Response) {
+    if (!request.cookies.token && !request.signedCookies.token)
+      return response.redirect('/login')
+
+    return { ...request.signedCookies, ...request.cookies };
   }
 
   @Get('login')
@@ -24,15 +27,31 @@ export class AppController {
     @Body() credencial: { username: string; password: string },
     @Res() response: Response,
   ) {
-    if (credencial.username === 'admin' && credencial.password === 'admin') {
-      return response
-        .cookie('token', 'imagine um token importante aqui', {
-          sameSite: 'lax',
-        })
-        .cookie('login', credencial.username)
-        .redirect('/');
+    if (!credencial.username || !credencial.password)
+      return response.status(401).send();
+
+    const cookieOptions: CookieOptions = {
+      sameSite: 'strict',
+      secure: true,
+      httpOnly: true,
+      signed: true
     }
 
-    return response.status(401).send();
+    return response
+      .cookie('token', 'imagine um token importante aqui', cookieOptions)
+      .cookie('username', credencial.username, cookieOptions)
+      .cookie('password', credencial.username, cookieOptions)
+      .redirect('/');
+  }
+
+  @Post('logout')
+  logout(
+    @Res() response: Response,
+  ) {
+    return response
+      .clearCookie('token')
+      .clearCookie('username')
+      .clearCookie('password')
+      .redirect('/login');
   }
 }
